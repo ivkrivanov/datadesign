da2f6a863350a83cb385846dd29d55e49ac9aa0e
Ivan Ivanov <ivan.kr.ivanov@hotmail.com>
2026-02-08 21:00:03 +0200

    SQL scripts

---
+++ 2026/Company/SQLScripts/DocumentType.sql
@@ -0,0 +1,39 @@
+﻿USE [Company_Sales_v1]
+GO
+
+/****** Object:  Table [sales].[DocumentType]    Script Date: 8.2.2026 г. 20:56:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [sales].[DocumentType](
+	[DocumentTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[DocumentName] [nvarchar](50) NOT NULL,
+	[InsertDate] [datetime] NOT NULL,
+	[InsertUserId] [int] NOT NULL,
+	[UpdateDate] [datetime] NULL,
+	[UpdateUserId] [int] NULL,
+	[IsActive] [int] NULL,
+	[TenantId] [int] NULL,
+ CONSTRAINT [PK_DocumentType] PRIMARY KEY CLUSTERED 
+(
+	[DocumentTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [sales].[DocumentType] ADD  DEFAULT (getdate()) FOR [InsertDate]
+GO
+
+ALTER TABLE [sales].[DocumentType] ADD  DEFAULT ((1)) FOR [InsertUserId]
+GO
+
+ALTER TABLE [sales].[DocumentType] ADD  DEFAULT ((1)) FOR [IsActive]
+GO
+
+ALTER TABLE [sales].[DocumentType] ADD  DEFAULT ((1)) FOR [TenantId]
+GO
+
+
---
+++ 2026/Company/SQLScripts/OperationType.sql
@@ -0,0 +1,40 @@
+﻿USE [Company_Sales_v1]
+GO
+
+/****** Object:  Table [sales].[OperationType]    Script Date: 8.2.2026 г. 20:53:01 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [sales].[OperationType](
+	[OperationTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[OpCode] [smallint] NOT NULL,
+	[Operation] [nvarchar](30) NOT NULL,
+	[InsertDate] [datetime] NOT NULL,
+	[InsertUserId] [int] NOT NULL,
+	[UpdateDate] [datetime] NULL,
+	[UpdateUserId] [int] NULL,
+	[IsActive] [int] NOT NULL,
+	[TenantId] [int] NOT NULL,
+ CONSTRAINT [PK_OperationType] PRIMARY KEY NONCLUSTERED 
+(
+	[OperationTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [sales].[OperationType] ADD  DEFAULT (getdate()) FOR [InsertDate]
+GO
+
+ALTER TABLE [sales].[OperationType] ADD  DEFAULT ((1)) FOR [InsertUserId]
+GO
+
+ALTER TABLE [sales].[OperationType] ADD  DEFAULT ((1)) FOR [IsActive]
+GO
+
+ALTER TABLE [sales].[OperationType] ADD  DEFAULT ((1)) FOR [TenantId]
+GO
+
+
